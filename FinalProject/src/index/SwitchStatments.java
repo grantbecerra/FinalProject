936431a452f60ed3f05b517eeeb2357c20e62fb2
GardnerSteven <31359320+GardnerSteven@users.noreply.github.com>
2017-10-10 08:55:30 -0500

    update switch

--- FinalProject/src/index/SwitchStatments.java
+++ FinalProject/src/index/SwitchStatments.java
@@ -21,7 +21,32 @@ case 137431:
 case 10:
 	System.out.println("You have returned American Assassin");
 	break;
-
+case 20:
+	System.out.println("You have returned American Assassin");
+	break;
+case 30:
+	System.out.println("You have returned My Little Pony: The Movie");
+	break;
+case 40:
+	System.out.println("You have returned Blade Runner 2049");
+	break;
+case 50:
+	System.out.println("You have returned American Made");
+	break;
+case 60:
+	System.out.println("You have returned The Mountain Between Us");
+	break;
+case 70:
+	System.out.println("You have returned Happy Death Day");
+	break;
+case 80:
+	System.out.println("You have returned Brave");
+	break;
+case 90:
+	System.out.println("You have returned Spider-Man: Homecoming");
+	break;
+case 100:
+	System.out.println("You have returned ");
 }
 	}
 }
